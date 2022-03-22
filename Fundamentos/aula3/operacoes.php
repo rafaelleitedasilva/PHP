d01396652e9badaa2058570d939aa1bd74f2fec5
Rafael leite <rafael.leite.14@hotmail.com>
2022-03-22 20:32:45 -0300

    Operações

---
+++ Fundamentos/aula3/operacoes.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade =25; $idadeDaqui10Anos = $idade +10;
+$idade5anosatras = $idade - 5;
+echo $idadeDaqui10Anos, " e ";
+echo $idade5anosatras;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2/2;
+$doisAoCubo = 2** 2;
+$restoDaDivisao = 10 % 3;
+
+
