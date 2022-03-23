67847b6d45de3b5dc105c4a586e4d883bc2ea23b
Rafael leite <rafael.leite.14@hotmail.com>
2022-03-22 21:09:56 -0300

    Lidando com Strings
    Como pular linha e colocar variáveis em textos.

---
+++ Fundamentos/aula5/quebra_de_linha.php
@@ -0,0 +1,11 @@
+<?php
+$idade = 18;
+
+//Aspas duplas também permitem a identificação de caracteres especiais
+
+//. PHP_EOL é uma segunda forma de pular linha.
+echo "Olá, Mundo! \n". PHP_EOL;
+//"Contra barra t" dá tab
+echo "\t";
+//O PHP tem dezenas de caracteres espciais
+echo "Olá, me chamo Rafael. Tenho $idade anos.";
---
+++ Fundamentos/aula5/strings.php
@@ -0,0 +1,5 @@
+<?php
+$idade = 18;
+//Aspas duplas permite o php identificar variáveis no meio da string
+echo "Olá, me chamo Rafael. Tenho $idade anos.";
+
