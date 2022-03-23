d96b78c8e2a22ba10ff85d69e2e9f04829710cba
Rafael leite <rafael.leite.14@hotmail.com>
2022-03-22 22:36:11 -0300

    Operador Ternário e Elseif

--- Fundamentos 2/aula6/condicao2.php
+++ Fundamentos 2/aula6/condicao2.php
@@ -10,11 +10,10 @@ if ($idade >= 18){
     echo "Você tem $idade anos". PHP_EOL;
     echo "Pode Entrar.";
 }
-else if ($idade >= 16 && $numeroDePessoas>1){
-
+elseif ($idade >= 16 && $numeroDePessoas>1){
     echo "Você tem $idade anos e está acompanhado(a).". PHP_EOL;
     echo "Pode Entrar.";
 }        
 else{
-   echo "Você não tem idade suficiente ainda, volte mais tarde.";
+    echo "Você não tem idade suficiente ainda, volte mais tarde.";
 }
---
+++ Fundamentos 2/aula6/operador_ternario.php
@@ -0,0 +1,4 @@
+<?php
+$idade = 15;
+$mensagem = $idade < 18 ? 'Você é menor de idade' : 'Você é maior de idade';
+echo $mensagem;
